2a6e4404497543f8cc73b4414d0837e069ca0500
keshavapatteri <mktkid2023@gmail.com>
2025-05-02 21:52:21 +0530

    sdfsdfsdf

--- Router/index.js
+++ Router/index.js
@@ -1,19 +1,20 @@
 import express from 'express'
 import UserRouter from './UserRouter.js';
-import AdminRouter from './adminRouter.js';
+
 import RestaurantRouter from './RestaurantRouter.js';
 import ProductRouter from './ProductRouter.js';
 import ReviewRouter from './ReviewRouter.js';
 import CartRouter from './CartRouter.js';
 import AddressRouter from './AddressRouter.js';
 import PaymentRouter from './PaymentRouter.js';
+import AdminRouter from './AdminRouter.js';
 
 
 const v1Router = express.Router();
 
 v1Router.use('/user',UserRouter)
 
-v1Router.use('/admin',AdminRouter)
+v1Router.use('/admin',AdminRouter)       
 
 v1Router.use('/restaurant',RestaurantRouter)
 
